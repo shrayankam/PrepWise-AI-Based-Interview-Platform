310e4bd563b0d55d0f80871ccffb519e371a1913
Shrayanka Mallick <mallickshrayanka72@gmail.com>
2025-09-25 02:17:47 +0530

    Updated

--- firebase/client.ts
+++ firebase/client.ts
@@ -8,9 +8,9 @@ import {getStorage} from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  apiKey: process.env.FIREBASE_PRIVATE_KEY,
+  authDomain: process.env.FIREBASE_CLIENT_EMAIL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
